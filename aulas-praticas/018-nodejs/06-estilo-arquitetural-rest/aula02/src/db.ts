1d13758fb05df6798050c092e5424a72a6f596df
JoaoVictorGuiziGerardi <jggerardi@gmail.com>
2022-08-04 11:15:59 -0300

    Fix db

--- aulas-praticas/018-nodejs/06-estilo-arquitetural-rest/aula02/src/db.ts
+++ aulas-praticas/018-nodejs/06-estilo-arquitetural-rest/aula02/src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connectionString = 'postgres://yhqirell:KcfLYuVz1ctN1fYq5sayU7EbV54mp_O6@kesavan.db.elephantsql.com/yhqirell';
+const connectionString = '';
 
 const db = new Pool({connectionString});
 
